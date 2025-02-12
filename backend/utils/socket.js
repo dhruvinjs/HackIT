46bc113b98a33f0ee0d721c4ed08e57a2457c535
dhruvinjs <dhruvin1055@gmail.com>
2025-02-13 00:54:46 +0530

    added the web rtc logic in Backend sokcet.js file

--- backend/utils/socket.js
+++ backend/utils/socket.js
@@ -2,6 +2,8 @@ import {Server} from "socket.io"
 import http from 'http'
 import express from "express"
 import Organization from "../models/organization.model.js";
+import { TeamModel } from "../models/team.model.js";
+import { asyncHandler } from "./Asynchandler.js";
 
 const app = express();
 const server = http.createServer(app);
@@ -15,6 +17,10 @@ const userSocketMap = {}
 export function getReceiverSocketId (userId){
     return userSocketMap[userId]
 }
+
+function generateRoomId(){  
+    return Math.floor(100000 + Math.random() * 900000);
+                         }
 
 
 io.on("connection",(socket)=>{
@@ -37,6 +43,33 @@ io.on("connection",(socket)=>{
    })
 
    
+   //Meeting Logic
+   socket.on('host-meeting',async(teamId)=>{
+    const teamId=await TeamModel.findById(teamId)
+    if (!teamId) {
+        socket.emit("error", { message: "Invalid Team Id" });
+        return;
+      } 
+      const roomId=generateRoomId()
+      socket.join(meetingRoomId);
+      console.log(`New Meeting hosted in ${roomId} with ${teamId.name}`);
+      //This will emit the roomId to chatroom of teamMembers
+      io.to(teamId).emit('meeting-hosted',{roomId})
+   })
+
+
+   socket.on('join-meeting',(roomId)=>{
+    if(!roomId){
+        socket.emit('error',{message:"Room id not provided"})
+    }
+
+    socket.join(roomId)
+
+    io.to(roomId).emit('new-joinee',{message:"New Joinee just joined"})
+
+   })
+
+
     socket.on("disconnect",()=>{
         console.log("A user disconnected , ",socket.id)
         delete userSocketMap[userId];
